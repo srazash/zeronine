0745125fbf1b7ad148e3b7ee3c08afabe93e5dbe
Ryan Shaw-Harrison <srazash@protonmail.com>
2025-04-29 21:50:38 +0100

    Testing HttpRequest can handle string coming in at runtime

--- src/main.zig
+++ src/main.zig
@@ -4,12 +4,20 @@ const http = @import("http.zig");
 
 pub fn main() !void {
     const stdout = @import("std").io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
+
     const socket = try SocketConfig.Socket.init([_]u8{ 0, 0, 0, 0 }, 8080);
 
-    try stdout.print("Server address --> {any}\n", .{socket._address});
+    var buffer: [100]u8 = undefined;
 
-    const req = try http.HttpRequest.init("GET /index.html");
+    try stdout.print("Enter request >> ", .{});
+    _ = try stdin.readUntilDelimiterOrEof(buffer[0..], '\n');
+    try stdout.print("\n", .{});
+
+    const req = try http.HttpRequest.init(&buffer);
     try stdout.print("Parsed request --> method: {?}, url: {s}\n", .{ req._method, req._url });
+
+    try stdout.print("Server address --> {any}\n", .{socket._address});
 
     var server = try socket._address.listen(.{});
 

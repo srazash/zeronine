c2a0800ead49767b700e0d338be951119deeecb3
Ryan Shaw-Harrison <srazash@protonmail.com>
2025-05-12 17:45:34 +0100

    Updated executable name to zeronined (indicating it is intended to run as a service or daemon)

--- build.zig
+++ build.zig
@@ -12,7 +12,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "zeronine",
+        .name = "zeronined",
         .root_module = exe_mod,
     });
 

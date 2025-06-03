18487b89ae2b9cfb74e288cfa03b57cb6d2955e0
Ryan Shaw-Harrison <srazash@protonmail.com>
2025-06-03 21:49:50 +0100

    Added app.zig to return app logo and version

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
+const app = @import("app.zig");
+
 const SocketConf = @import("config.zig");
-const http = @import("http.zig");
+const Http = @import("http.zig");
 
 pub fn main() !void {
     const stdout = @import("std").io.getStdOut().writer();
@@ -9,7 +11,7 @@ pub fn main() !void {
     const socket = try SocketConf.Socket.init([_]u8{ 0, 0, 0, 0 }, 80);
 
     // var buffer: [256]u8 = undefined;
-    var req: http.HttpRequest = undefined;
+    var req: Http.HttpRequest = undefined;
 
     // try stdout.print("Enter request >> ", .{});
     // if (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |input| {
@@ -18,6 +20,8 @@ pub fn main() !void {
 
     // try stdout.print("Parsed request --> method: {?}, url: {s}\n", .{ req._method, req._url });
 
+    try stdout.print("{s}", .{app.app_logo});
+
     try stdout.print("Server address --> {any}\n", .{socket._address});
 
     var server = try socket._address.listen(.{});
@@ -25,8 +29,8 @@ pub fn main() !void {
     const connection = try server.accept();
     //_ = connection;
 
-    req = try http.HttpRequest.init(connection);
+    req = try Http.HttpRequest.init(connection);
     //try stdout.print("Request from connection --> {s}\n", .{conn_buffer});
     try stdout.print("http/0.9-ified request --> method: {?}, path: {s}\n", .{ req._method, req._path });
-    try stdout.print("file check --> {any}\n", .{http.HttpRequest.fileExists(req._path)});
+    try stdout.print("file check --> {any}\n", .{Http.HttpRequest.fileExists(req._path)});
 }

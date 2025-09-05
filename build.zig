4184e98e2b8be6a9ba3eb50043ca31792880f1e3
Ryan Shaw-Harrison <srazash@protonmail.com>
2025-09-05 11:30:40 +0100

    Updated to Zig 0.15.1, removed old http and config as these no longer compile, rewriting both modules

--- build.zig
+++ build.zig
@@ -1,31 +1,22 @@
 const std = @import("std");
-
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
-
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
 
     const exe = b.addExecutable(.{
         .name = "zeronined",
-        .root_module = exe_mod,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
-
     b.installArtifact(exe);
-
+    const run_step = b.step("run", "Run zeronined");
     const run_cmd = b.addRunArtifact(exe);
-
+    run_step.dependOn(&run_cmd.step);
     run_cmd.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,43 +1,18 @@
 const std = @import("std");
 const app = @import("app.zig");
 
-const Socket = @import("config.zig").Socket;
-const Http = @import("http.zig");
-
 pub fn main() !void {
-    const stdout = @import("std").io.getStdOut().writer();
-    // const stdin = std.io.getStdIn().reader();
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
 
-    const socket = try Socket.init([_]u8{ 0, 0, 0, 0 }, 80);
+    try stdout.print("{s}\n", .{app.logo});
+    try stdout.flush();
 
-    // var buffer: [256]u8 = undefined;
-    var req: Http.HttpRequest = undefined;
-
-    // try stdout.print("Enter request >> ", .{});
-    // if (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |input| {
-    //     req = try http.HttpRequest.init(input);
-    // }
-
-    // try stdout.print("Parsed request --> method: {?}, url: {s}\n", .{ req._method, req._url });
-
-    try stdout.print("{s}\nserver version: {s}\n{s}\n\n", .{ app.logo, app.version, app.line });
-
-    try stdout.print("server address --> {any}\n", .{socket._address});
-
-    var server = try socket._address.listen(.{});
+    std.log.info("starting zeronined v{s}", .{app.version});
+    std.log.info("ctrl+c to kill zeronined", .{});
 
     while (true) {
-        const connection = try server.accept();
-        //_ = connection;
-
-        req = try Http.HttpRequest.init(connection);
-        //try stdout.print("Request from connection --> {s}\n", .{conn_buffer});
-        try stdout.print("http/0.9-ified request --> method: {?}, path: {s}\n", .{ req._method, req._path });
-        //try stdout.print("file check --> {any}\n", .{Http.HttpRequest.fileExists(req._path)});
-
-        try Http.HttpRequest.sendResponse(connection);
-        try stdout.print("sending html response --> {s}\n", .{Http.response});
-
-        connection.stream.close();
+        // do http-y stuff
     }
 }

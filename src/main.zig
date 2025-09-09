df34745b066bc72e6f0c723df0ab9a924f2b7099
Ryan Shaw-Harrison <srazash@protonmail.com>
2025-09-09 20:16:55 +0100

    Server code moved to server.zig

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const app = @import("app.zig");
+const httpserver = @import("server.zig").HttpServer;
 
 pub fn main() !void {
     var stdout_buffer: [1024]u8 = undefined;
@@ -9,33 +10,6 @@ pub fn main() !void {
     try stdout.print("{s}\n", .{app.logo});
     try stdout.flush();
 
-    std.log.info("starting zeronined v{s}", .{app.version});
-    std.log.info("ctrl+c to kill zeronined", .{});
-
-    // test server code
-    const ipv4 = "0.0.0.0";
-    const port: u16 = 8080;
-
-    var readin: [1024]u8 = undefined;
-
-    const address = try std.net.Address.resolveIp(ipv4, port);
-    var server = try address.listen(.{ .reuse_address = true });
-    defer server.deinit();
-
-    std.log.info("listening on {s}:{d}", .{ ipv4, port });
-
-    while (true) {
-        const conn = try server.accept();
-
-        const end = try conn.stream.read(&readin);
-
-        std.log.info("<-REQ-IN- {s}", .{readin[0..end]});
-
-        try conn.stream.writeAll("RECIEVED\n");
-
-        std.log.info("-REQ-OUT-> {s}", .{"'RECIEVED'\n"});
-
-        conn.stream.close();
-        continue;
-    }
+    var server = httpserver.init("0.0.0.0", 8080);
+    try server.start();
 }
--- src/server.zig
+++ src/server.zig
@@ -1,3 +1,41 @@
 const std = @import("std");
+const app = @import("app.zig");
 
-const HttpServer = struct {};
+pub const HttpServer = struct {
+    ip: []const u8,
+    port: u16,
+
+    pub fn init(ip: []const u8, port: u16) HttpServer {
+        return HttpServer{
+            .ip = ip,
+            .port = port,
+        };
+    }
+
+    pub fn start(self: *HttpServer) !void {
+        var readin: [1024]u8 = undefined;
+
+        const address = try std.net.Address.resolveIp(self.ip, self.port);
+        var server = try address.listen(.{ .reuse_address = true });
+        defer server.deinit();
+
+        std.log.info("starting zeronined v{s}", .{app.version});
+        std.log.info("ctrl+c to kill zeronined", .{});
+        std.log.info("listening on {s}:{d}", .{ self.ip, self.port });
+
+        while (true) {
+            const conn = try server.accept();
+
+            _ = try conn.stream.read(&readin);
+
+            std.log.info("<-REQ-IN--", .{});
+
+            try conn.stream.writeAll("RECIEVED\n");
+
+            std.log.info("-RES-OUT->", .{});
+
+            conn.stream.close();
+            continue;
+        }
+    }
+};
